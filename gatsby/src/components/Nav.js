331702eb8ae473a6f02f5d623b99c7526932241c
Andrew <gskll@protonmail.com>
2020-12-07 16:22:43 +0100

    add styles to Layout

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -19,6 +19,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
 
   li {
@@ -45,6 +46,11 @@ const NavStyles = styled.nav`
     &:hover {
       color: var(--red);
     }
+    /* Highlight current page navlink 
+    * &[aria-current='page'] {
+    *   color: var(--red);
+    * }
+    */
   }
 `;
 
