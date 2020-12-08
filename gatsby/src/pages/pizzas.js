95bfcaa2b26e1a348f884a04ebb846528b5ea42b
Andrew <gskll@protonmail.com>
2020-12-08 14:29:44 +0100

    add pageQuery to fetch all pizzas - PizzaPage

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+
   return (
     <>
-      <p>This is the PizzasPage!</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const pageQuery = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
