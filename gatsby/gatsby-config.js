a64e5645fb4d6bf7805af9c155db2e9e3264f00b
Andrew <gskll@protonmail.com>
2020-12-19 17:54:54 +0100

    add twitter metadata and react-helmet plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,20 +1,22 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
 export default {
   siteMetadata: {
-    title: 'Slicks Slices',
-    siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place in Hamilton!',
+    title: "Slicks Slices",
+    siteUrl: "https://gatsby.pizza",
+    description: "The best pizza place in Hamilton!",
+    twitter: "@slickslices",
   },
   plugins: [
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
-        projectId: 'qgjkjaa4',
-        dataset: 'production',
+        projectId: "qgjkjaa4",
+        dataset: "production",
         watchMode: true,
         token: process.env.SANITY_TOKEN,
       },
