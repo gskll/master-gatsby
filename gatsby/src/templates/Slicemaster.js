18fa6cbed6e0494c4577759a83aa0ff279d92f9c
Andrew <gskll@protonmail.com>
2020-12-19 16:47:26 +0100

    add single slicemaster page template

---
+++ gatsby/src/templates/Slicemaster.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Img from "gatsby-image";
+import { graphql } from "gatsby";
+
+export default function SingleSlicemasterPage({ data: { slicemaster } }) {
+  return (
+    <div className="center">
+      <Img fluid={slicemaster.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{slicemaster.name}</h2>
+        <p>{slicemaster.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    slicemaster: sanityPerson(slug: { current: { eq: $slug } }) {
+      name
+      id
+      description
+      image {
+        asset {
+          fluid(maxWidth: 1000, maxHeight: 750) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`;
+
