58f10e7ff7f4fdcfe0be196fa46f5d97b335f38a
Andrew <gskll@protonmail.com>
2020-12-21 14:22:20 +0100

    update storeSettings schema to hold name, slices

--- sanity/schemas/storeSettings.js
+++ sanity/schemas/storeSettings.js
@@ -9,10 +9,22 @@ export default {
   icon,
   fields: [
     {
+      name: "name",
+      title: "Store Name",
+      type: "string",
+      description: "Name of the Store",
+    },
+    {
       name: "slicemaster",
       title: "Slicemasters currently Slicing",
       type: "array",
       of: [{ type: "reference", to: [{ type: "person" }] }],
+    },
+    {
+      name: "hotSlices",
+      title: "Hot Slices available in the case",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "pizza" }] }],
     },
   ],
 };
