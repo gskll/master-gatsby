f6c1cef22b3747b30108f66cdba95b7b61be8acc
Andrew <gskll@protonmail.com>
2020-12-21 10:22:16 +0100

    wrap nodemailer in exports.handler to send mail

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -9,12 +9,21 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  // Workaround for ethereal dodgy certificate
+  tls: { rejectUnauthorized: false },
 });
 
-// Test send an email
-transporter.sendMail({
-  from: "Slick's Slices <slick@example.com>",
-  to: "orders@example.com",
-  subject: "New order!",
-  html: `<p>Your new pizza order is here</p>`,
-});
+exports.handler = async (event, context) => {
+  // Test send an email
+  const info = await transporter.sendMail({
+    from: "Slick's Slices <slick@example.com>",
+    to: "orders@example.com",
+    subject: "New order!",
+    html: `<p>Your new pizza order is here</p>`,
+  });
+  console.log(">>>>>>", info);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  };
+};
