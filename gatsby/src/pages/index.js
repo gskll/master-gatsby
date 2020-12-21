afc15034fbb7db4d31b31800829c590e086836e5
Andrew <gskll@protonmail.com>
2020-12-21 15:08:56 +0100

    use custom hook to fetch data on homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import useLatestData from "../utils/useLatestData";
+
+function CurrentlySlicing({ slicemasters }) {
+  return <div>CurrentlySlicing</div>;
+}
+
+function HotSlices({ hotSlices }) {
+  return <div>HotSlices</div>;
+}
 
 export default function HomePage() {
+  const { hotSlices, slicemasters } = useLatestData();
+
   return (
-    <>
-      <p>This is the homepage!</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters}></CurrentlySlicing>
+        <HotSlices hotSlices={hotSlices}></HotSlices>
+      </div>
+    </div>
   );
 }
