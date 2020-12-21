10e88e3e44d51a66cfda4e1778debd634fc915c1
Andrew <gskll@protonmail.com>
2020-12-21 11:53:04 +0100

    implement order send with placeOrder function

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,23 @@
 const nodemailer = require("nodemailer");
 
+function generateOrderEmail({ order, total }) {
+  return `
+    <div>
+      <h2>Your Recent Order for ${total}</h2>
+      <p>Please start walking over,  we will have your order ready in the next 20 mins.</p>
+      <ul>
+        ${order.map(
+          (item) =>
+            `<li>
+            <img src="${item.thumbnail}" alt="${item.name}">
+            ${item.size} ${item.name} - ${item.price}
+          </li>`
+        )}
+      </ul>
+      <p>Your total is $${total} due at pickup</p>
+    </div>
+  `;
+}
 // create a transport for nodemailer
 // sendgrid or postmark or ethereal.email for testing
 const transporter = nodemailer.createTransport({
@@ -34,16 +52,14 @@ exports.handler = async (event, context) => {
   }
   // Send the email
   // Send the success or error message
-  // Test send an email
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@example.com>",
-    to: "orders@example.com",
+    to: `${body.name} <${body.email}>`,
     subject: "New order!",
-    html: `<p>Your new pizza order is here</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
-  console.log(">>>>>>", info);
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: "Success" }),
   };
 };
